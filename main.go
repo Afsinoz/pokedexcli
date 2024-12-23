aeed64f1ece27b66a7c712de35d39c67063e34a2
Afsinoz <afsinozdemir@gmail.com>
2024-12-23 10:47:52 +0100

    Adding basic scan to cli

--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"fmt"
 	"strings"
+	"bufio"
+	"os"
 )
-
 
 func cleanInput(text string) []string {
 	splittedString := strings.Fields(text)
@@ -12,6 +13,18 @@ func cleanInput(text string) []string {
 }
 
 func main() {
-	text := " hello world  "
-	fmt.Println(text)
+	scanner := bufio.NewScanner(os.Stdin)
+	for true{
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		userInput := scanner.Text()
+		userInput = strings.ToLower(userInput)
+		userInputList := cleanInput(userInput)
+		firstWord := userInputList[0]
+		fmt.Println("Your command was:",firstWord)
+	}
+	if err := scanner.Err() ; err != nil {
+		fmt.Fprintln(os.Stderr, "reading the std input:", err)
+	}
+	
 }
